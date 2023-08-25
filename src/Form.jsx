a098aa04c482290649940de9856f0d3882f2350b
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2023-08-25 06:24:12 +0200

    practice

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,4 +1,43 @@
+import { useState } from 'react';
+
 const Form = () => {
-  return <h1>Form</h1>;
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+  });
+
+  console.log(formData);
+
+  const handleChange = (event) => {
+    setFormData((prevFormData) => {
+      return { ...prevFormData, [event.target.name]: event.target.value };
+    });
+  };
+  return (
+    <form>
+      <input
+        type="text"
+        placeholder="First Name"
+        onChange={handleChange}
+        name="firstName"
+        value={formData.firstName}
+      />
+      <input
+        type="text"
+        placeholder="Last Name"
+        onChange={handleChange}
+        name="lastName"
+        value={formData.lastName}
+      />
+      <input
+        type="email"
+        placeholder="Email"
+        onChange={handleChange}
+        name="email"
+        value={formData.email}
+      />
+    </form>
+  );
 };
 export default Form;
